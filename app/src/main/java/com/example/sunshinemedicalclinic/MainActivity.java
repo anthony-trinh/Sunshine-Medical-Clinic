d969adc6ed52cf08d18aae6e181b2b466025283e
amiel <amiel.castillo16@gmail.com>
2020-11-08 15:49:09 -0500

    added splash with audio, and icon on actionbar

--- app/src/main/java/com/example/sunshinemedicalclinic/MainActivity.java
+++ app/src/main/java/com/example/sunshinemedicalclinic/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Show app icon on action bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
     }
 }
--- app/src/main/java/com/example/sunshinemedicalclinic/SplashScreen.java
+++ app/src/main/java/com/example/sunshinemedicalclinic/SplashScreen.java
@@ -2,13 +2,31 @@ package com.example.sunshinemedicalclinic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class SplashScreen extends AppCompatActivity {
+MediaPlayer openingSound = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        openingSound = MediaPlayer.create(this, R.raw.whoosh);
+        openingSound.start();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                openingSound.stop();
+                finish();
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        };
+
+        Timer opening = new Timer();
+        opening.schedule(task, 3500);
     }
 }
